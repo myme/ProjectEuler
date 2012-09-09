56daf364da44b7d3890a0ace4684b90f1324132e
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2012-09-09 22:01:55 +0200

    Add comments

--- Lib.hs
+++ Lib.hs
@@ -1,16 +1,21 @@
 module Lib where
 
+-- | Fibonacci sequence, starting from [1, 1, .. ].
 fibs :: [Integer]
 fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
 
+-- | Find all integer divisors of a number.
 divisors :: Integer -> [Integer]
 divisors x = [y | y <- [2 .. floor . sqrt . fromIntegral $ x], x `mod` y == 0]
 
+-- | Returns true if integer is a prime number.
 isPrime :: Integer -> Bool
 isPrime = null . divisors
 
+-- | Finds all prime factors of an integer.
 primeFactors :: Integer -> [Integer]
 primeFactors = filter isPrime . divisors
 
+-- | Returns true if the provided string is a palindrome.
 isPalindrome :: String -> Bool
 isPalindrome s = s == reverse s
